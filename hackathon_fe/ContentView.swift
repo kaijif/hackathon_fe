c63f246033fb604d9fa14edc5350f621148dee13
kaijif <thefuka2@gmail.com>
2026-06-23 19:47:23 -0700

    Initial Commit

---
+++ hackathon_fe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hackathon_fe
+//
+//  Created by Kaiji Fu on 6/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hackathon_fe/hackathon_feApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathon_feApp.swift
+//  hackathon_fe
+//
+//  Created by Kaiji Fu on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct hackathon_feApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
